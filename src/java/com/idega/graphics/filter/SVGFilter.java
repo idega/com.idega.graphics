3519ab6154c4b12a9bcd6bcdc71ac35af8a82247
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-01-26 03:26:07 +0000

    cleanup and javadoc update

--- src/java/com/idega/graphics/filter/SVGFilter.java
+++ src/java/com/idega/graphics/filter/SVGFilter.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SVGFilter.java,v 1.5 2005/01/21 13:53:44 tryggvil Exp $
+ * $Id: SVGFilter.java,v 1.6 2005/01/26 03:26:07 tryggvil Exp $
  * Created on 18.7.2004 by Tryggvi Larusson
  *
  * Copyright (C) 2004-2005 Idega Software hf. All Rights Reserved.
@@ -23,12 +23,12 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * <p>
  * Filter that acts as a renderer of SVG files and uses Batik to render either a PNG or JPEG image.<br>
- * This filter is mapped by default on urls with the patterns *.svg,*.svg.jsp and *.svg.jspx
+ * This filter is mapped by default on urls with the patterns *.svg,*.jsvg,*.svg.jsp and *.svg.jspx
  * </p>
- *  Last modified: $Date: 2005/01/21 13:53:44 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2005/01/26 03:26:07 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class SVGFilter implements Filter {
 	
@@ -59,33 +59,21 @@ public class SVGFilter implements Filter {
 		// TODO Auto-generated method stub
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
-	 *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
-	 */
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		if (req instanceof HttpServletRequest) {
 			HttpServletRequest request = (HttpServletRequest) req;
 			HttpServletResponse response = (HttpServletResponse) res;
-			//String ae = request.getHeader("accept-encoding");
-			//if (ae != null && ae.indexOf("gzip") != -1) {
-				//System.out.println("GZIP supported, compressing.");
-				String outputFormat = getOutputFormatForClient(request);
-				if(outputFormat.equals(FORMAT_SVG)){
-					//just bypass this filter to do the default handing:
-					chain.doFilter(req,res);
-				}
-				else{
-					//else wrap the response:
-					SVGFilterResponseWrapper wrappedResponse = new SVGFilterResponseWrapper(response,request,outputFormat);
-					chain.doFilter(req, wrappedResponse);
-					wrappedResponse.finishResponse();
-				}
-				//return;
-			//}
-			//chain.doFilter(req, res);
+			String outputFormat = getOutputFormatForClient(request);
+			if(outputFormat.equals(FORMAT_SVG)){
+				//just bypass this filter to do the default handing:
+				chain.doFilter(req,res);
+			}
+			else{
+				//else wrap the response:
+				SVGFilterResponseWrapper wrappedResponse = new SVGFilterResponseWrapper(response,request,outputFormat);
+				chain.doFilter(req, wrappedResponse);
+				wrappedResponse.finishResponse();
+			}
 		}
 	}
 
@@ -106,38 +94,4 @@ public class SVGFilter implements Filter {
 		}
 		return FORMAT_PNG;
 	}
-
-
-	/*
-	public void emitJPG(HttpServletRequest request,
-			HttpServletResponse response, String svgString) {
-		response.setContentType("image/jpeg");
-		JPEGTranscoder t = new JPEGTranscoder();
-		t.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, new Float(.8));
-		TranscoderInput input = new TranscoderInput(new StringReader(svgString));
-		try {
-			TranscoderOutput output = new TranscoderOutput(response
-					.getOutputStream());
-			t.transcode(input, output);
-			response.getOutputStream().close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void emitPNG(HttpServletRequest request,
-			HttpServletResponse response, String svgString) {
-		response.setContentType("image/png");
-		PNGTranscoder t = new PNGTranscoder();
-		TranscoderInput input = new TranscoderInput(new StringReader(svgString));
-		try {
-			TranscoderOutput output = new TranscoderOutput(response
-					.getOutputStream());
-			t.transcode(input, output);
-			response.getOutputStream().close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	*/
 }
