1905a351d243023cfd672d0f054f0e8e72997cab
Valdas Zemaitis <valdas@idega.com>
2008-01-10 17:02:52 +0000

    New constants

--- src/java/com/idega/graphics/business/GraphicsConstants.java
+++ src/java/com/idega/graphics/business/GraphicsConstants.java
@@ -4,5 +4,8 @@ public class GraphicsConstants {
 	
 	public static final String JPG_FILE_NAME_EXTENSION = "jpg";
 	public static final String PNG_FILE_NAME_EXTENSION = "png";
+	
+	protected static final int GENERATED_IMAGE_HEIGHT = 1024;
+	protected static final int GENERATED_IMAGE_WIDTH = 768;
 
 }
