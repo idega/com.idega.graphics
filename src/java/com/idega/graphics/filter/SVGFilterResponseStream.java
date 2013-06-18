47d9fac1a6728220cb992afd1a1fc0ba0bd253e4
Valdas Žemaitis <valdas@idega.com>
2013-06-18 12:11:25 +0300

    Using constant

--- src/java/com/idega/graphics/filter/SVGFilterResponseStream.java
+++ src/java/com/idega/graphics/filter/SVGFilterResponseStream.java
@@ -5,9 +5,11 @@ package com.idega.graphics.filter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.JPEGTranscoder;
@@ -39,6 +41,7 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 		this.outputFormat=outputFormat;
 	}
 
+	@Override
 	public void close() throws IOException {
 		if (this.closed) {
 			throw new IOException("This output stream has already been closed");
@@ -47,12 +50,12 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 
 		//byte[] bytes = buffer.toByteArray();
 		//String svgString = new String(bytes);
-		
-		
-		//response.addHeader("Content-Length", Integer.toString(bytes.length));
+
+
+		//response.addHeader(CoreConstants.PARAMETER_CONTENT_LENGTH, Integer.toString(bytes.length));
 		//response.setContentType("image/png");
 		//response.addHeader("Content-Encoding", "gzip");
-		
+
 		if(this.outputFormat.equals(SVGFilter.FORMAT_PNG)){
 			emitPNG(this.output);
 		}
@@ -61,14 +64,14 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 		//}
 		else if(this.outputFormat.equals(SVGFilter.FORMAT_JPEG)){
 			emitJPEG(this.output);
-		}		
-		
+		}
+
 		/*output.write(bytes);
 		output.flush();
 		output.close();*/
 		this.closed = true;
 	}
-	
+
 	public void emitPNG(ServletOutputStream output) {
 		this.response.setContentType("image/png");
 		PNGTranscoder t = new PNGTranscoder();
@@ -99,15 +102,15 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}	
-	
+	}
+
 	/*
 	public void emitSVG(ServletOutputStream output, String svgString) {
 		response.setContentType("image/svg+xml");
 		try {
 			//response.getWriter().write(svgString);
 			//response.getWriter().flush();
-			
+
 			OutputStreamWriter writer = new OutputStreamWriter(output);
 			writer.write(svgString);
 			//output.write(new StringReader(svgString));
@@ -118,12 +121,13 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 			e.printStackTrace();
 		}
 	}*/
-	
+
 	protected String getRequestedUri(){
 		StringBuffer url = this.request.getRequestURL();
 		return url.toString();
 	}
-	
+
+	@Override
 	public void flush() throws IOException {
 		if (this.closed) {
 			throw new IOException("Cannot flush a closed output stream");
@@ -131,6 +135,7 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 		this.buffer.flush();
 	}
 
+	@Override
 	public void write(int b) throws IOException {
 		if (this.closed) {
 			throw new IOException("Cannot write to a closed output stream");
@@ -138,10 +143,12 @@ public class SVGFilterResponseStream extends ServletOutputStream {
 		this.buffer.write((byte) b);
 	}
 
+	@Override
 	public void write(byte b[]) throws IOException {
 		write(b, 0, b.length);
 	}
 
+	@Override
 	public void write(byte b[], int off, int len) throws IOException {
 		//System.out.println("writing...");
 		if (this.closed) {
