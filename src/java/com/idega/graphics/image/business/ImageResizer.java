814929a130b099731315a7d6cb75d4582b06324b
Valdas Žemaitis <valdas@idega.com>
2013-08-21 10:11:43 +0300

    New method (to scale image down only if it is bigger); image resizer now returns InputStream

--- src/java/com/idega/graphics/image/business/ImageResizer.java
+++ src/java/com/idega/graphics/image/business/ImageResizer.java
@@ -2,18 +2,16 @@ package com.idega.graphics.image.business;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import com.idega.business.SpringBeanName;
 
 @SpringBeanName("imageResizer")
 public interface ImageResizer {
 
-	public OutputStream getScaledImage(int newWidth, int newHeight, InputStream streamToImage, String imageType) throws IOException;
+	public InputStream getScaledImage(int newWidth, int newHeight, InputStream streamToImage, String imageType) throws IOException;
 
-	public OutputStream getScaledImage(int minSize, InputStream streamToImage, String imageType) throws IOException;
+	public InputStream getScaledImage(int minSize, InputStream streamToImage, String imageType) throws IOException;
 
-	public OutputStream getScaledImage(int newWidth, int newHeight, InputStream streamToImage, String imageType, OutputStream outputStream)
-			throws IOException;
+	public InputStream getScaledImageIfBigger(int newSize, InputStream streamToImage, String imageType) throws IOException;
 
 }
--- src/java/com/idega/graphics/image/business/impl/ImageResizerImpl.java
+++ src/java/com/idega/graphics/image/business/impl/ImageResizerImpl.java
@@ -1,10 +1,10 @@
 package com.idega.graphics.image.business.impl;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
@@ -19,23 +19,33 @@ import com.idega.util.StringUtil;
 public class ImageResizerImpl extends DefaultSpringBean implements ImageResizer {
 
 	@Override
-	public OutputStream getScaledImage(int newWidth, int newHeight, InputStream streamToImage, String imageType) throws IOException {
-		return getScaledImage(newWidth, newHeight, streamToImage, imageType, new ByteArrayOutputStream());
-	}
-
-	@Override
-	public OutputStream getScaledImage(int newWidth, int newHeight, InputStream streamToImage, String imageType, OutputStream outputStream)
-			throws IOException {
+	public InputStream getScaledImage(int newWidth, int newHeight, InputStream streamToImage, String imageType) throws IOException {
 		if (newWidth < 0 || newHeight < 0 || streamToImage == null || StringUtil.isEmpty(imageType)) {
 			getLogger().warning("Invalid parameters!");
 			return null;
 		}
 
-		return getScaledImage(newWidth, newHeight, -1, streamToImage, imageType, outputStream);
+		return getScaledImage(newWidth, newHeight, -1, streamToImage, imageType, null);
 	}
 
-	private OutputStream getScaledImage(int newWidth, int newHeight, int minSize,
-			InputStream streamToImage, String imageType, OutputStream outputStream) throws IOException {
+	@Override
+	public InputStream getScaledImage(int minSize, InputStream streamToImage, String imageType) throws IOException {
+		return getScaledImage(-1, -1, minSize, streamToImage, imageType, null);
+	}
+
+	@Override
+	public InputStream getScaledImageIfBigger(int newSize, InputStream streamToImage, String imageType) throws IOException {
+		return getScaledImage(-1, -1, newSize, streamToImage, imageType, newSize);
+	}
+
+	private InputStream getScaledImage(
+			int newWidth,
+			int newHeight,
+			int minSize,
+			InputStream streamToImage,
+			String imageType,
+			Integer scaleIfBiggerThan
+	) throws IOException {
 
 		if (streamToImage == null) {
 			getLogger().warning("Stream to image is not provided!");
@@ -44,33 +54,42 @@ public class ImageResizerImpl extends DefaultSpringBean implements ImageResizer 
 
 		int originalWidth = -1, originalHeight = -1;
 		long start = System.currentTimeMillis();
+		ByteArrayOutputStream output = null;
 		try {
 			BufferedImage image = ImageIO.read(streamToImage);
 
-			if (newWidth < 0 && newHeight < 0 && minSize > 0) {
-				originalWidth = image.getWidth();
-				originalHeight = image.getHeight();
+			originalHeight = image.getHeight();
+			boolean scale = true;
+			if (scaleIfBiggerThan != null && originalHeight <= scaleIfBiggerThan) {
+				scale = false;
+			}
 
-				double ratio = minSize * 1.0 / originalWidth;
-				newHeight = Double.valueOf(originalHeight * ratio).intValue();
-				if (newHeight < minSize) {
-					ratio = minSize * 1.0 / originalHeight;
+			BufferedImage scaled = null;
+			if (scale) {
+				if (newWidth < 0 && newHeight < 0 && minSize > 0) {
+					originalWidth = image.getWidth();
+
+					double ratio = minSize * 1.0 / originalWidth;
+					newHeight = Double.valueOf(originalHeight * ratio).intValue();
+					if (newHeight < minSize) {
+						ratio = minSize * 1.0 / originalHeight;
+					}
+
+					newHeight = Double.valueOf(originalHeight * ratio).intValue();
+					newWidth = Double.valueOf(originalWidth * ratio).intValue();
 				}
 
-				newHeight = Double.valueOf(originalHeight * ratio).intValue();
-				newWidth = Double.valueOf(originalWidth * ratio).intValue();
+				scaled = Scalr.resize(image, Method.SPEED, newWidth, newHeight, Scalr.OP_ANTIALIAS, Scalr.OP_BRIGHTER);
+			} else {
+				scaled = image;
 			}
 
-			BufferedImage scaled = Scalr.resize(image, Method.SPEED, newWidth, newHeight, Scalr.OP_ANTIALIAS, Scalr.OP_BRIGHTER);
-
-			if (outputStream == null) {
-				outputStream = new ByteArrayOutputStream();
-			}
-
-			ImageIO.write(scaled, imageType, outputStream);
-			return outputStream;
+			output = new ByteArrayOutputStream();
+			ImageIO.write(scaled, imageType, output);
+			return new ByteArrayInputStream(output.toByteArray());
 		} finally {
 			IOUtil.close(streamToImage);
+			IOUtil.close(output);
 
 			long duration = System.currentTimeMillis() - start;
 			if (duration > 300) {
@@ -78,11 +97,6 @@ public class ImageResizerImpl extends DefaultSpringBean implements ImageResizer 
 					newWidth + "x" + newHeight);
 			}
 		}
-	}
-
-	@Override
-	public OutputStream getScaledImage(int minSize, InputStream streamToImage, String imageType) throws IOException {
-		return getScaledImage(-1, -1, minSize, streamToImage, imageType, new ByteArrayOutputStream());
 	}
 
 }
